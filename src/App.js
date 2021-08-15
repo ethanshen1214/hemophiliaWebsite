aefb9a608722671bc672a61b0222309212676120
Ethan Shen <ethanshen1214@gmail.com>
2021-08-15 00:35:09 -0500

    add hemophilia logo to footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Navbar, Jumbotron, Container } from 'react-bootstrap';
+import { Navbar, Jumbotron, Container, Row, Col, } from 'react-bootstrap';
 import { HashRouter as Router} from 'react-router-dom';
 import Routes from './components/routes';
 import './App.css';
@@ -36,14 +36,32 @@ class App extends Component {
 
             <Jumbotron id="footer" jumbotron-fluid>
                     <Container id="jumboText">
-                        <p>
-                          Monday - Friday<br/>
-                          8:00 am—5:00 pm<br/>
-                          (214) 648-5177<br/>
-                          (214) 648-1937<br/>
-                          (214) 648-3049<br/>
-                          Fax: (214)-648-4105
-                        </p>
+                      <Row>
+                        <Col md={{ span: 4, offset: 2 }}>
+                          <br/>
+                          <img
+                            alt=""
+                            src="/logo.jpg"
+                            //width="250"
+                            //height="50"
+                            // hspace="130"
+                            style={{marginLeft: "16%", marginRight: "50px"}}
+                            className="d-inline-block align-top"
+                          />{' '}
+                        </Col>
+                        <Col md={{ span: 4, offset: 0 }}>
+                          <p>
+                            Monday - Friday<br/>
+                            8:00 am—5:00 pm<br/>
+                            (214) 648-5177<br/>
+                            (214) 648-1937<br/>
+                            (214) 648-3049<br/>
+                            Fax: (214)-648-4105
+                          </p>
+                        </Col>
+                      </Row>
+                    
+                        
                     </Container>
             </Jumbotron>
           </div>
